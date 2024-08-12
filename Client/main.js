faedfb6bc9b88be9486ab7d4e927df3f5a479b68
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-08-12 20:34:56 +0300

    add

--- Client/main.js
+++ Client/main.js
@@ -1,5 +1,7 @@
 const serverPath = 'http://localhost:5000';
 const _searchBtn = document.getElementById('searchBtn');
+const _mainTable = document.getElementById('main-table');
+const _list = document.getElementById('list');
 _searchBtn.addEventListener('click', handleSearchClick);
 
 
@@ -15,5 +17,42 @@ function getData(){
 }
 
 async function handleSearchClick(){
-    console.log(await getData());
+    const data = await getData();
+    console.log(data);
+    displayTable(data);
 }
+
+function displayTable(data){
+    const rows = data.recordset;
+    console.log(rows);
+    const numOfCols = Object.keys(rows[0]).length;
+
+    const tableHead = document.createElement("tr");
+    _mainTable.appendChild(tableHead);
+
+    for(const [key,value] of Object.entries(rows[0])){
+        const col = document.createElement("th");
+        col.textContent = key;
+        tableHead.appendChild(col);
+    }
+
+    for(let i=0;i<rows.length;i++){
+        const tableRow = document.createElement("tr");
+        _mainTable.appendChild(tableRow);
+        for(const [key,value] of Object.entries(rows[i])){
+            const cell = document.createElement("td");
+            cell.textContent = value;
+            tableRow.appendChild(cell);
+        }
+    }
+
+}
+
+function updateList (){
+    for(let i=0;i<5;i++){
+        const item = document.createElement("li");
+        item.textContent = i;
+        _list.appendChild(item);
+    }
+}
+// updateList();
