1aaa24f5557b8c24fec306d9729704187b0805c2
Amit Kubani <amitkubani@gmail.com>
2024-08-14 14:48:57 +0300

    basic server login

--- Client/main.js
+++ Client/main.js
@@ -1,8 +1,11 @@
 //general elements
 const serverPath = 'http://localhost:5000';
+const fullPath = window.location.pathname;
+const pageName = fullPath.substring(fullPath.lastIndexOf('/') + 1);
 const _loggedUser = document.getElementById('loggedUser');
+
 const _state = {
-    currentPage : 'index',
+    currentPage : pageName,
     currentUser : 'guest',
     refreshView : function(){
         _loggedUser.textContent = `Hello ${this.currentUser}`;
@@ -19,19 +22,51 @@ let _inputUserName;
 let _inputPass = document.getElementById('inputPass');
 
 
-function updateState(currentPage, currentUser){
-    if(currentPage) _state.currentPage = currentPage;
+function updateLocalState(currentUser){
     if(currentUser) _state.currentUser = currentUser;
-    if(_state.currentPage === 'index'){
+    if(_state.currentPage === 'index.html'){
         _searchBtn.addEventListener('click', handleSearchClick);
     }
-    else if(_state.currentPage === 'login'){
+    else if(_state.currentPage === 'login.html'){
         _inputUserName = document.getElementById('inputUserName');
         _inputPass = document.getElementById('inputPass');
 
     }    
     _state.refreshView();
 }
+
+function getState(){
+    fetch(`${serverPath}/api/data/state`)
+    .then(response => response.json())
+    .then(state => {
+      updateLocalState(state.currentUser);
+    })
+    .catch(error => console.log(error));
+}
+
+function checkLogin(){
+    const user = _inputUserName.value;    
+    const pass = _inputPass.value;
+    console.log("user",user);
+    console.log("pass", pass);
+    fetch(`${serverPath}/api/login`,{
+        method: 'POST',
+        headers:{
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+            userName: user,
+            password: pass
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.log(error));
+}
+
+getState();
 
 function getData(){
     const tableName = _inputSearch.value;
@@ -76,27 +111,4 @@ function displayTable(data){
             tableRow.appendChild(cell);
         }
     }
-}
-
-function checkLogin(){
-    const user = _inputUserName.value;    
-    const pass = _inputPass.value;
-    fetch(`${serverPath}/api/data/Logins`, {
-        headers: {
-            "Content-Type":"application/json"
-        },
-        method: "GET"
-    })
-    .then(response => response.json())
-    .then(data => {
-        const users = data.recordset;
-        
-        for(let i=0;i<users.length;i++){
-            if(users[i].UserName === user && users[i].Password === pass){
-                _isLoggedIn = true;                 
-                break;
-            }
-        }
-    })
-    .catch(error => console.log(error));
 }
--- Server/server.js
+++ Server/server.js
@@ -1,35 +1,58 @@
+//server members
 const sql = require('mssql');
 const fs = require('fs');
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const config = JSON.parse(fs.readFileSync('./config.txt','utf-8'));
+const config = JSON.parse(fs.readFileSync('./config.txt', 'utf-8'));
 const port = 5000;
 
-app.use(cors());
-app.use(express.json());
-app.get('/api/data/:tableName', (req, res) => {
-    console.log(req);
-    const { tableName } = req.params;
-    sql.query(`SELECT TOP 20 * FROM ${tableName}`)
-    .then(result => res.json(result))
-    .catch(error => {
-        console.log(error);
-        res.status(500).send("duck error in the server...");
-    });
-});
-app.get('/api/data/')
+//client members
+const state = {//use with primitives only!(only a shallow copy is sent to client...)
+    currentUser: 'guest'
+}
 
-function connectToDB(){
-    try{
+
+function connectToDB() {
+    try {
         sql.connect(config);
         console.log('The server connected to the DB...');
     }
-    catch(error){
+    catch (error) {
         console.log(`error connecting to db:${error}`);
     }
 }
-app.listen(port,()=>{
+
+
+app.use(cors());
+app.use(express.json());
+app.get('/api/data/state', (req, res) => {
+    res.json({ ...state });
+});
+
+app.get('/api/data/:tableName', (req, res) => {
+    const { tableName } = req.params;
+    sql.query(`SELECT TOP 20 * FROM ${tableName}`)
+        .then(result => res.json(result))
+        .catch(error => {
+            console.log(error);
+            res.status(500).send("duck error in the server...");
+        });
+});
+
+app.post('/api/login', async function (req,res){
+    req = req.body;
+    const userName = req.userName;
+    const password = req.password;
+
+    let user = await sql.query(`SELECT * FROM logins WHERE UserName = '${userName}' AND Password = '${password}'`);
+    user = user.recordset[0];
+    
+    if(user) res.json('login successful');
+    else res.json('login failed');
+});
+
+app.listen(port, () => {
     connectToDB();
     console.log(`The server has started listening on port ${port}...`);
 });
